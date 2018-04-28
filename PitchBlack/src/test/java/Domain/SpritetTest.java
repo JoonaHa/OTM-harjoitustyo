1b5cace3ab0c24822f0c2d08d28b47acc1a187dd
Mina <mina@localhost.localdomain>
2018-04-28 18:00:53 +0300

    Finished SpriteTest

--- PitchBlack/src/test/java/Domain/SpritetTest.java
+++ PitchBlack/src/test/java/Domain/SpritetTest.java
@@ -6,12 +6,14 @@
 package Domain;
 
 import javafx.geometry.Point2D;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
-import org.junit.After;
+import javafx.scene.shape.Shape;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 import pitchblack.domain.Sprite;
+import pitchblack.gui.Ui;
 
 /**
  *
@@ -64,11 +66,46 @@ public class SpritetTest {
         sprite.rotateTowards(x, y);
 
         double angle = Math.toDegrees(Math.atan2(sprite.getShape().getTranslateY() - y,
-                x - sprite.getShape().getTranslateX())) * -1;
+                x - sprite.getShape().getTranslateX())) *-1;
 
         assertEquals(angle, sprite.getShape().getRotate(), 0);
     }
 
+    @Test
+    public void updateSetsRightcoordinates() {
+        double x = 5;
+        double y = 10;
 
+        sprite.setVelocity(new Point2D(x, y));
+
+        sprite.update();
+
+        assertEquals(x, sprite.getShape().getTranslateX(), 0);
+        assertEquals(y, sprite.getShape().getTranslateY(), 0);
+
+    }
+
+    @Test
+    public void intersectsReturnsTrue() {
+        Shape test = new Circle(10, 10, 10);
+
+        test.setTranslateX(this.sprite.getShape().getTranslateX());
+        test.setTranslateY(this.sprite.getShape().getTranslateY());
+
+        assertEquals(true, sprite.intersects(test));
+
+    }
+    
+    
+    @Test
+    public void intersectsReturnsFalse() {
+        Shape test = new Circle(2, 2, 2);
+
+        test.setTranslateX(100);
+        test.setTranslateY(100);
+
+        assertEquals(false, sprite.intersects(test));
+
+    }
 
 }
