14c540bb180cb33c8cba4bcc5a1fd879006c44f7
Mina <mina@localhost.localdomain>
2018-05-03 02:02:52 +0300

    Create database if it's missing and add example data

--- PitchBlack/src/main/java/pitchblack/database/Database.java
+++ PitchBlack/src/main/java/pitchblack/database/Database.java
@@ -1,42 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pitchblack.database;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import pitchblack.dao.ScoresDao;
+import pitchblack.domain.Score;
 
 /**
- *Luokka hallitsee tietokantayhteyttä.
+ * Luokka hallitsee tietokantayhteyttä ja luo tietokannan jos sitä ei ole.
+ *
  * @author JoonaHa
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  */
 public class Database {
 
     private String databaseAddress;
 
     /**
-     * 
+     *
      * @param databaseAddress Käytettävän tietokannan osoite.
-     * 
+     *
      */
-    public Database(String databaseAddress)  {
+    public Database(String databaseAddress) throws SQLException {
         this.databaseAddress = databaseAddress;
+
+        File f = new File("highScores.db");
+
+        if (!f.exists()) {
+            initDatabase();
+        }
     }
 
     /**
-     * 
+     *
      * @return Muodostaa yhteyden tietokantaan ja palauttaa Connection-olion.
-     * @throws SQLException 
+     * @throws SQLException
      */
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(databaseAddress);
+    }
+
+    private void initDatabase() throws SQLException {
+        try (Connection conn = DriverManager.getConnection(databaseAddress);
+                PreparedStatement stmt = conn.prepareStatement("CREATE TABLE Scores (id serial PRIMARY KEY, score integer, name varchar(100))")) {
+
+            stmt.execute();
+
+            stmt.close();
+
+            conn.close();
+            
+            // Add test data
+            ScoresDao sc = new ScoresDao(this);
+            sc.add(new Score("Impossible", -1));
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
 }
