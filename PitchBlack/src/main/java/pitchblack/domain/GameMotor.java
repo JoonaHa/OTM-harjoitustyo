2ac4e11e68ab30a00b82353d5eba4f5e3036e8d9
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2018-05-09 23:04:08 +0300

    Added basic spawn system

--- PitchBlack/src/main/java/pitchblack/domain/GameMotor.java
+++ PitchBlack/src/main/java/pitchblack/domain/GameMotor.java
@@ -3,6 +3,9 @@ package pitchblack.domain;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
+import java.util.Stack;
+import java.util.concurrent.TimeUnit;
 import javafx.geometry.Point2D;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
@@ -22,24 +25,28 @@ public class GameMotor {
     private static GameMotor gameMotor;
     private final HashMap<KeyCode, Boolean> input;
     private final HashMap<MouseButton, Boolean> mouseClicks;
-    private final ArrayList<MouseEvent> mouseEvents;
+    private final Stack<MouseEvent> mouseMovements;
     private Player player;
+    private List<Sprite> enemies;
     private Boolean running;
     private int score;
+    private long spawnTime;
     private List<Bullet> bullets;
 
     /**
      *
      *
      */
-    private GameMotor(HashMap<KeyCode, Boolean> input, ArrayList<MouseEvent> mouseEvents, HashMap<MouseButton, Boolean> mouseClicks) {
+    private GameMotor(HashMap<KeyCode, Boolean> input, Stack<MouseEvent> mouseMovements, HashMap<MouseButton, Boolean> mouseClicks) {
         this.input = input;
         this.mouseClicks = mouseClicks;
-        this.mouseEvents = mouseEvents;
+        this.mouseMovements = mouseMovements;
         this.player = new Player(WIDTH / 2, HEIGHT / 2);
         this.player.rotateTowards(WIDTH / 2, 0);
+        this.enemies = new ArrayList<>();
         this.running = true;
         this.score = 0;
+        this.spawnTime = TimeUnit.SECONDS.convert(System.nanoTime(), TimeUnit.NANOSECONDS);
         this.bullets = new ArrayList<>();
 
     }
@@ -48,12 +55,12 @@ public class GameMotor {
      * Luokka palauttaa GameMotor instanssin.
      *
      * @param input Käyttäjän näppäimistö painallukset.
-     * @param mouseEvents Käyttäjän hiiren tapahtumat.
+     * @param mouseMovements Käyttäjän hiiren tapahtumat.
      * @return GameMotor instanssi.
      */
-    public static GameMotor getInstance(HashMap<KeyCode, Boolean> input, ArrayList<MouseEvent> mouseEvents, HashMap<MouseButton, Boolean> mouseClicks) {
+    public static GameMotor getInstance(HashMap<KeyCode, Boolean> input, Stack<MouseEvent> mouseMovements, HashMap<MouseButton, Boolean> mouseClicks) {
         if (gameMotor == null) {
-            gameMotor = new GameMotor(input, mouseEvents, mouseClicks);
+            gameMotor = new GameMotor(input, mouseMovements, mouseClicks);
         }
         return gameMotor;
     }
@@ -90,21 +97,92 @@ public class GameMotor {
 
         }
 
-        if (mouseClicks.getOrDefault(MouseButton.PRIMARY, Boolean.FALSE && bullets.size() < 1)) {
+        if (mouseClicks.getOrDefault(MouseButton.PRIMARY, Boolean.FALSE && bullets.size() < 3)) {
             Bullet bullet = new Bullet(player);
             bullets.add(bullet);
             bullet.shoot();
 
-            Ui.render(bullet.getShape());
+            Ui.addNodeToGame(bullet.getShape());
 
         }
 
-        if (mouseEvents.size() > 0) {
-            player.rotateTowards(mouseEvents.get(mouseEvents.size() - 1).getSceneX(), mouseEvents.get(mouseEvents.size() - 1).getSceneY());
+        if (mouseMovements.size() > 0) {
+            player.rotateTowards(mouseMovements.get(mouseMovements.size() - 1).getSceneX(), mouseMovements.get(mouseMovements.size() - 1).getSceneY());
         }
+
+        long compTime = TimeUnit.SECONDS.convert(System.nanoTime(), TimeUnit.NANOSECONDS);
+
+        if (compTime - this.spawnTime >= 6) {
+            spawnEnemies();
+             this.spawnTime = TimeUnit.SECONDS.convert(System.nanoTime(), TimeUnit.NANOSECONDS);
+        }
+
+        bullets.forEach(bullet -> {
+
+            enemies.stream().filter(enemy -> enemy.intersects(bullet.getShape())).forEach(enemy -> {
+                this.score ++;
+                bullet.setAlife(false);
+                enemy.setAlife(false);
+                
+            });
+
+            if (!bullet.isAlife()) {
+                Ui.removeNodeFromGame(bullet.getShape());
+                bullets.remove(bullet);
+
+            }
+        });
+
+        enemies.forEach(enemy -> {
+            if (!enemy.isAlife()) {
+                Ui.removeNodeFromGame(enemy.getShape());
+                enemies.remove(enemy);
+
+            }
+        });
 
         bullets.forEach(bullet -> bullet.update());
+        enemies.forEach(enemy -> enemy.patrol(getPlayer()));
+        enemies.forEach(enemy -> enemy.update());
         player.update();
+
+    }
+
+    private void spawnEnemies() {
+        int enemy1Count = 3 + score;
+        int enemy2Count = 0 + score ;
+
+        for (int i = 0; i < enemy1Count; i++) {
+
+            // spawn enemy offscreen but still on their acceptable locations
+            double xSpawnArea = Enemy1.borderOffset;
+            double ySpawnArea = Enemy1.borderOffset;
+
+            double x = xSpawnArea * new Random().nextDouble() + Ui.WIDTH;
+            double y = ySpawnArea * new Random().nextDouble() + Ui.HEIGHT;
+
+            Enemy1 enemy1 = new Enemy1(x, y);
+
+            this.enemies.add(enemy1);
+            Ui.addNodeToGame(enemy1.getShape());
+
+        }
+
+        for (int i = 0; i < enemy2Count; i++) {
+
+            // spawn enemy offscreen but still in to their acceptable locations
+            double xSpawnArea = Enemy1.borderOffset;
+            double ySpawnArea = Enemy1.borderOffset;
+
+            double x = xSpawnArea * new Random().nextDouble() + Ui.WIDTH;
+            double y = ySpawnArea * new Random().nextDouble() + Ui.HEIGHT;
+
+            Enemy2 enemy2 = new Enemy2(x, y);
+
+            this.enemies.add(enemy2);
+            Ui.addNodeToGame(enemy2.getShape());
+
+        }
 
     }
 
@@ -116,7 +194,7 @@ public class GameMotor {
         return score;
     }
 
-    public List<Bullet> getBullets() {
+    private List<Bullet> getBullets() {
         return bullets;
     }
 
@@ -141,9 +219,13 @@ public class GameMotor {
     public void newGame() {
         this.player = new Player(WIDTH / 2, HEIGHT / 2);
         this.player.rotateTowards(WIDTH / 2, 0);
+        Ui.addNodeToGame(this.player.getShape());
+        this.enemies = new ArrayList<>();
+        this.running = true;
         this.score = 0;
         this.bullets = new ArrayList<>();
         this.resume();
+        spawnEnemies();
 
     }
 
